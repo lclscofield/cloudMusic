70a5c9bc66588c704ed2d066b4c376a99e8f35bb
lclscofield <1479904979@qq.com>
2017-09-13 17:24:26 +0800

    优化搜索逻辑 优先显示歌手，没有找到歌手则显示歌曲

--- cloudMusic.js
+++ cloudMusic.js
@@ -329,23 +329,26 @@ function showSearchHint(value) {
     h3.classList.add('active')
     h3.innerHTML = `搜索“${value}”`
     // 获取提示结果并渲染到页面
-    let query1 = new AV.Query('Song')
-    query1.contains('name', value)
-    query1.find().then(function (results) {
-      for (let i = 0; i < results.length; i++) {
-        let song = results[i].attributes
-        let li = `<li><i></i><span>${song.name}</span></li>`
-        searchHintUl.insertAdjacentHTML('beforeend', li)
-      }
-    })
     let query2 = new AV.Query('Song')
     query2.contains('singer', value)
-    // let query = AV.Query.or(query1, query2)
     query2.find().then(function (results) {
-      for (let i = 0; i < results.length; i++) {
-        let song = results[i].attributes
-        let li = `<li><i></i><span>${song.singer}</span></li>`
-        searchHintUl.insertAdjacentHTML('beforeend', li)
+      console.log(results)
+      if (results.length !== 0) {
+        for (let i = 0; i < results.length; i++) {
+          let song = results[i].attributes
+          let li = `<li><i></i><span>${song.singer}</span></li>`
+          searchHintUl.insertAdjacentHTML('beforeend', li)
+        }
+      } else {
+        let query1 = new AV.Query('Song')
+        query1.contains('name', value)
+        query1.find().then(function (results) {
+          for (let i = 0; i < results.length; i++) {
+            let song = results[i].attributes
+            let li = `<li><i></i><span>${song.name}</span></li>`
+            searchHintUl.insertAdjacentHTML('beforeend', li)
+          }
+        })
       }
     })
   }
