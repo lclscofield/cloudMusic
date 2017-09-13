de64d9d465baa56d42fd101931255272417d37b9
lclscofield <1479904979@qq.com>
2017-09-13 17:14:38 +0800

    优化搜索歌手和搜索歌曲的逻辑 搜索歌手时显示歌手结果 搜索歌曲时显示歌曲结果 如果都有则都显示

--- cloudMusic.js
+++ cloudMusic.js
@@ -331,13 +331,20 @@ function showSearchHint(value) {
     // 获取提示结果并渲染到页面
     let query1 = new AV.Query('Song')
     query1.contains('name', value)
-    let query2 = new AV.Query('Song')
-    query2.contains('singer', value)
-    let query = AV.Query.or(query1, query2)
-    query.find().then(function (results) {
+    query1.find().then(function (results) {
       for (let i = 0; i < results.length; i++) {
         let song = results[i].attributes
-        let li = `<li><i></i><span>${song.name} - ${song.singer}</span></li>`
+        let li = `<li><i></i><span>${song.name}</span></li>`
+        searchHintUl.insertAdjacentHTML('beforeend', li)
+      }
+    })
+    let query2 = new AV.Query('Song')
+    query2.contains('singer', value)
+    // let query = AV.Query.or(query1, query2)
+    query2.find().then(function (results) {
+      for (let i = 0; i < results.length; i++) {
+        let song = results[i].attributes
+        let li = `<li><i></i><span>${song.singer}</span></li>`
         searchHintUl.insertAdjacentHTML('beforeend', li)
       }
     })
