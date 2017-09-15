5c4cfa14e896e2c435e37a73a06c4ac212cbd22a
lclscofield <1479904979@qq.com>
2017-09-15 17:57:10 +0800

    添加歌词滚动效果

--- song.js
+++ song.js
@@ -17,37 +17,85 @@ function getParameterByName(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-
+// 找到 id 对应歌曲并播放
 let id = getParameterByName('id')
 let query = new AV.Query('Song')
 query.get(id).then(function (song) {
   let {
+    name,
+    singer,
     url,
     lyric
   } = song.attributes
-  let video = document.createElement('video')
-  video.src = url
-  video.play()
   let arr = []
-  let parts = lyric.split('\n')
+  let parts = lyric.split('\n') // 截取每段歌词
+
   parts.forEach(function (string, index) {
     let str = string.split(']')
     str[0] = Number(str[0].slice(1, str[0].indexOf(':'))) * 60 + Number(str[0].slice(str[0].indexOf(':') + 1))
     arr.push({
       time: str[0],
       lyric: str[1]
-    })
+    }) // 把每段歌词放到数组中
   })
-  console.log(arr)
-  setInterval(function () {
+  // 加载歌曲名和歌手
+  let h2 = document.querySelector('.song-content h2')
+  h2.innerHTML = `${name} - <span>${singer}</span>`
+  // 加载歌词
+  let inner = document.querySelector('.song-content .song-lyric-inner')
+  for (let i = 0; i < arr.length; i++) {
+    inner.insertAdjacentHTML('beforeend', `<p>${arr[i].lyric}</p>`)
+  }
+  // 点击播放按钮播放
+  let playBtn = document.querySelector('.top span.playBtn')
+  playBtn.addEventListener('click', function () {
+    playBtn.classList.remove('pause')
+    play(url, arr, inner, playBtn)
+  })
+  // 播放
+  play(url, arr, inner, playBtn)
+})
+
+// 播放
+function play(url, arr, inner, playBtn) {
+  // 视频播放
+  let video = document.createElement('video')
+  video.src = url
+  video.play()
+  // 封面旋转
+  let cover = document.querySelector('.top .cover')
+  cover.classList.add('playing')
+  // 歌词滚动
+  let p = document.querySelectorAll('.song-content .song-lyric-inner p')
+  p[0].style.color = `rgb(255, 255, 255)`
+  let time = setInterval(function () {
     let current = video.currentTime
     for (let i = 0; i < arr.length; i++) {
-      if (i === arr.length) {
-        console.log(arr[i].lyric)
-      } else if (arr[i].time <= current && arr[i + 1].time > current) {
-        console.log(arr[i].lyric)
+      if (i === arr.length - 1) {
+        if (current >= arr[i].time) {
+          p[i - 1].style.color = ``
+          p[i].style.color = `rgb(255, 255, 255)`
+          inner.style.transform = `translateY(-${(i-1)*32}px)`
+        }
+      } else
+      if (current >= arr[i].time && current < arr[i + 1].time) {
+        if (i === 0) {
+          p[i].style.color = `rgb(255, 255, 255)`
+        } else {
+          p[i - 1].style.color = ``
+          p[i].style.color = `rgb(255, 255, 255)`
+          inner.style.transform = `translateY(-${(i-1)*32}px)`
+        }
         break
       }
     }
-  }, 500)
-})
+    if (current === video.currentTime) {
+      p[0].style.color = `rgb(255, 255, 255)`
+      inner.style.transform = `translateY(0px)`
+      cover.classList.remove('playing')
+      cover.classList.add('initial')
+      playBtn.classList.add('pause')
+      clearInterval(time)
+    }
+  }, 300)
+}
